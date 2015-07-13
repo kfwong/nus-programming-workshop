2ebeb71f8cbc78b801aa9b9d718893eb7beb42f9
kfwong <kfwong@zoho.com>
2015-07-13 13:49:46 +0800

    Added mind reading exercise solution.

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # nus-programming-workshop
 Programming Refresher Workshop Exercises
+
+http://www.comp.nus.edu.sg/~tantc/refresher/
+
+##Objectives
+The workshop aims to provide participants a refresher on programming and problem-solving skills covered in the first programming module (CS1010 or its equivalent). It is designed for incoming students holding polytechnic diploma who have been exempted from CS1010 (Programming Methodology) or its equivalent.
+At the end of this 3-day workshop, participants will have a better appreciation of the programming skills taught in CS1010. They will also gain experience in using some of the main problem-solving skills covered in CS1010.
+
+##Dates and Venues
+Dates: 13, 14, 16 July 2015 
+Lecture venue: SR3 (COM1-02-12) 
+Lab venue: PL2 (COM1-B-09)
---
+++ exe1-mind-reading/Main.java
@@ -0,0 +1,69 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		ArrayList<int[]> numList = new ArrayList<int[]>();
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		while(scanner.hasNext()){
+						
+			int[] entry = new int[3];
+			
+			entry[0] = scanner.nextInt();
+			
+			if( entry[0] == -1){
+				break;
+			}
+			
+			entry[1] = scanner.nextInt();
+			entry[2] = scanner.nextInt();
+			
+			numList.add(entry);
+			
+		}
+		
+		for(int[] nums : numList){
+			System.out.println(findHiddenNum(nums));
+		}
+		
+		
+	}
+	
+	private static int findHiddenNum(int... numbers){
+		int digitalRoot = 0;
+		for(int number: numbers){
+			digitalRoot = digitalRoot + number;
+		}
+		
+		if(digitalRoot < 10){
+			
+			if (digitalRoot == 9){
+				return 9;
+			}
+			return 9 - digitalRoot;
+		}else{
+		
+			return findHiddenNum(toIntArray(digitalRoot));
+		}
+	}
+	
+	private static int[] toIntArray(int num){
+		char[] numChars = String.valueOf(num).toCharArray();
+		int[] nums = new int[numChars.length];
+		
+		for(int i = 0;i<numChars.length;i++){
+			nums[i] = Integer.parseInt(Character.toString(numChars[i]));
+		}
+		return nums;
+	}
+
+}
+
+
